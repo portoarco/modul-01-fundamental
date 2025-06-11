43b74a7742dd854a06d898a7aa241b9febe44f5e
portoarco <arcoanggoro78@gmail.com>
2025-06-11 13:34:05 +0700

    Pembahasan dan Latihan OOP

--- Day07/exercise1.ts
+++ Day07/exercise1.ts
@@ -120,56 +120,86 @@ console.log(cekUmur());
 // PEMBAHASAN EXERCISE 1  = cara MAS BAGAS 
 
 interface Student {
-    name:string,
-    email:string,
-    age:Date,
-    score:number;
+    name: string,
+    email: string,
+    age: Date,
+    score: number
 }
+
 interface StatResult {
-    highest:number,
-    lowest:number,
-    average:number
+    highest: number,
+    lowest: number,
+    average: number
 }
+
 interface StudentStatsResult {
     score: StatResult,
-    age:StatResult
+    age: StatResult
 }
 
-class StudentStatsCalculator{
-    private student:Student[]
+class StudentStatsCalculator {
+    private student: Student[]
 
-    constructor(params: {students: Student[]}){
-        this.student = params.students
+    constructor(student: Student[]) {
+        this.student = student
     }
 
-    private calculateAge(birthDate:Date){
-        const today = new Date();
-        const age = today.getFullYear()-birthDate.getFullYear()
-        const hasHadBirthdayThisYear = today.getMonth() > birthDate.getMonth() ||
-            (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate())
-        return hasHadBirthdayThisYear ? age : age-1
-    }
-    private calculateStats(values:number[]){
-        const highest = Math.max(...values);
-        const lowest = Math.min(...values);
+    private calculateAge(birthDate: Date) {
+        const today = new Date()
+        const actualBirthDate = new Date(birthDate)
 
-        const average = values.reduce((acc,val) => acc + val,0) / values.length
+
+        const age = today.getFullYear() - actualBirthDate.getFullYear()
+        const hasHadBirthdayThisYear = today.getMonth() > actualBirthDate.getMonth() ||
+            (today.getMonth() === actualBirthDate.getMonth() && today.getDate() >= actualBirthDate.getDate())
+        return hasHadBirthdayThisYear ? age : age - 1
+    }
+
+    private calculateStats(values: number[]) {
+        const highest = Math.max(...values)
+        const lowest = Math.min(...values)
+
+        const average = values.reduce((acc, val) => acc + val, 0) / values.length
+
         return {
-            highest,lowest,average:parseFloat(average.toFixed(2))
+            highest, lowest, average: parseFloat(average.toFixed(2))
         }
     }
 
-    public getStats(){
-        const scores = this.student.map((student) => student.score);
-        const ages = this.student.map((student) => this.calculateAge(student.age))
-        
+    public getStats() {
+        const scores = this.student.map((student) => student.score)
+        const ages: any = this.student.map((student) => this.calculateAge(student.age))
+
         return {
-        score: this.calculateStats(scores),
-        age: this.calculateAge(ages)
-    }
+            score: this.calculateStats(scores),
+            age: this.calculateStats(ages)
+        }
     }
 
-    
 }
+
+const listStudent: Student[] = [
+    {
+        name: 'Alice',
+        email: 'alice@example.com',
+        age: new Date('2002-06-10'),
+        score: 88
+    },
+    {
+        name: 'Bob',
+        email: 'bob@example.com',
+        age: new Date('2001-07-08'),
+        score: 92
+    },
+    {
+        name: 'Charlie',
+        email: 'charlie@example.com',
+        age: new Date('2001-05-10'),
+        score: 75
+    }
+]
+
+// const calculator = new StudentStatsCalculator(listStudent)
+// console.log(calculator.getStats())
 
 
--- Day07/exercise2.ts
+++ Day07/exercise2.ts
@@ -16,21 +16,21 @@ EXERCISE 2
     ○ Checkout method → Finalize transaction, return transaction data
 */ 
 
-class ProductInfo{
-    prdName:string;
-    prdPrice:number;
+// class ProductInfo{
+//     prdName:string;
+//     prdPrice:number;
 
-    constructor(prdName:string,prdPrice:number){
-        this.prdName = prdName
-        this.prdPrice = prdPrice
-    }
+//     constructor(prdName:string,prdPrice:number){
+//         this.prdName = prdName
+//         this.prdPrice = prdPrice
+//     }
 
-    addtoCart(prdName:string,prdPrice:number){
-        this.prdPrice += prdPrice
-    }
-    showTotal(){}
-    coPayment(){}
-}
+//     addtoCart(prdName:string,prdPrice:number){
+//         this.prdPrice += prdPrice
+//     }
+//     showTotal(){}
+//     coPayment(){}
+// }
 
 
 // class TransactionInfo{
@@ -41,8 +41,99 @@ class ProductInfo{
 
 // }
 
-const product1 = new ProductInfo ('Mi Goreng',20000)
-const product2 = new ProductInfo ('Mi Jamur',15000)
-console.log(product1); //testing 1   
+// const product1 = new ProductInfo ('Mi Goreng',20000)
+// const product2 = new ProductInfo ('Mi Jamur',15000)
+// console.log(product1); //testing 1   
+
+
+// PEMBAHASAN
+
+// nb. data_transaction -> snake case model penulisan
+// dataTransaction -> camel case model penulisan
+// DataTransaction -> pascal case model penulisan
+
+// class Product {
+//     constructor(name:string, price:number){
+
+//     }
+// }    native ways 
+
+interface IProduct {
+    name:string,
+    price:number
+}
+
+interface ICartItem {
+    product : Product,
+    quantity : number
+}
+
+
+
+class Product implements IProduct {
+    name:string
+    price:number
+
+    constructor(name:string,price:number){
+        this.name = name
+        this.price = price
+    }
+}
+
+class Transaction {
+    private cart:ICartItem[] = []
+
+    // tambah produk ke cart
+    public addToCart(product:Product, quantity:number){
+        const existingItem = this.cart.find((item) => item.product.name === product.name)
+
+        if(existingItem){
+            existingItem.quantity += quantity
+        } else {
+            this.cart.push({product,quantity})
+        }
+
+
+    }
+
+    // show total
+    public showTotal(){
+        return this.cart.reduce((total,item)=> total + item.product.price * item.quantity,0)
+    }
+
+    // checkout dan kembalikan detail dari transaksi (bila sudah terbayar, maka keranjang direset jadi 0)
+    public checkout(){
+        const total = this.showTotal()
+        const items = [...this.cart]
+
+        this.cart = []
+        return {items,total}
+    }
+}
+
+const product1 = new Product ('Laptop',1500)
+const product2 = new Product ('Mouse',50)
+const product3 = new Product ('Tas',100)
+
+const transaction = new Transaction()
+
+transaction.addToCart(product1,1)
+transaction.addToCart(product2,2)
+transaction.addToCart(product3,1)
+
+console.log("Current Total:", transaction.showTotal());
+
+const result = transaction.checkout()
+
+console.log("Transaction Summary:  ");
+
+result.items.forEach((item) => {
+  console.log(`${item.product.name} x ${item.quantity} = $ ${item.product.price} * ${item.quantity}`);
+    
+})
+
+console.log("Total $",result.total);
+
+
 
 
